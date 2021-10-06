5beb63674d6d836beee460d735c18b68fdd91f34
pmad83 <piotrekmadej@gmail.com>
2021-10-06 13:48:46 +0200

    3 files modified

--- src/app/cennik-dostaw/cennik-dostaw.component.ts
+++ src/app/cennik-dostaw/cennik-dostaw.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 import { KoszykService } from '../koszyk.service';
 
--- src/app/koszyk/koszyk.component.ts
+++ src/app/koszyk/koszyk.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 import { KoszykService } from '../koszyk.service';
+import { CennikDostawComponent } from '../cennik-dostaw/cennik-dostaw.component';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-koszyk',
@@ -8,11 +15,29 @@ import { KoszykService } from '../koszyk.service';
   styleUrls: ['./koszyk.component.css'],
 })
 export class KoszykComponent {
+
+  //animal: string;
+  //name: string;
+
   items = this.koszyk.dajKoszyk();
   total = this.items.reduce((total, current) => total + current.price, 0);
   deliveryCost = 0;
   valueWithDelivery = this.total + this.deliveryCost;
 
-  constructor(private koszyk: KoszykService) {}
+  constructor(
+              //public dialog: MatDialog,
+              private koszyk: KoszykService) {}
+
+  /*openDialog(): void {
+    const dialogRef = this.dialog.open(CennikDostawComponent, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }*/
 
 }
